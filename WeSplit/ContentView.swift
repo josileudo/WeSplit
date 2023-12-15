30ad39f2c17ae80d43e714456b3f54da40de09ff
Josileudo <josileudorodrigues75@gmail.com>
2023-12-15 16:06:09 -0300

    :memo: label: Create form using picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,23 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2.0
+    @State private var numberOfPeople = 2
     @State private var tipPercent = 20
     
     var body: some View {
-        Form {
-            Section {
-                TextField(
-                    "Check amount: ",
-                    value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField(
+                        "Check amount: ",
+                        value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")
                         
-                )
-                .keyboardType(.decimalPad)
+                    )
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of peoples", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
